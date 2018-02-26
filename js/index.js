44e20a3a5ba762205c3e15a9e435bb538f431d31
Tech-A41 <tech-a41@Tech-A41s-MacBook-Air.local>
2018-02-26 13:09:29 +0000

    get the user input into the console

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// alert("Working"); // Test to see JS is working
+// match the word from the user input 
+var word = document.getElementById('wordword');
+
+
+// get the user input from HTML
+document.querySelector('form.wordform').addEventListener('submit', function(event) {
+	event.preventDefault();
+	var value = word.value
+	console.log(value)
+	// clearing the input box
+	word.value = "";
+});

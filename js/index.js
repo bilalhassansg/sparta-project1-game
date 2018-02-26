80c552b10eb951ca16d272a40a029f2e06c07084
Tech-A41 <tech-a41@Tech-A41s-MacBook-Air.local>
2018-02-26 15:00:25 +0000

    chage h2 to random word from array

--- js/index.js
+++ js/index.js
@@ -5,11 +5,16 @@ var word = document.getElementById('wordword');
 var wordsarray = ['fuzzy', 'pizza' ,'quick', 'jumbo', 'chuck', 'jelly', 'joker', 'bumpy', 'prize', 'equals', 'reject', 'wizard', 'injury', 'mumble', 'jazzman', 'puzzled', 'zombies', 'hammock', 'cutback', 'knockoff', 'junkyard', 'hijacker', 'juggling', 'equipped', 'magazine', 'optimize', 'knocking','projects', 'acquired', 'jellybean', 'misjudged', 'judgement', 'technique', 'enjoyment', 'privatize', 'irritated', 'restarted', 'retreated', 'resistant', 'adjustable', 'transporter', 'terminology', 'surrounding', 'specialisms'];
 
 
+var firstword = Math.floor(Math.random()*wordsarray.length);
+
+var match = $("h2").html(wordsarray[firstword]);
+
 // get the user input from HTML
 document.querySelector('form.wordform').addEventListener('submit', function(event) {
 	event.preventDefault();
 	var value = word.value;
 	console.log(value);
+
 	// clearing the input box
 	word.value = "";
 });
@@ -18,8 +23,24 @@ document.querySelector('form.wordform').addEventListener('submit', function(even
 
 // generate random word from array 
 
-// for (var i = 0; i < wordsarray.length; i++){
+for (var i = 0; i < wordsarray.length; i++){
 
-// 	randomword = wordsarray[Math.floor(Math.random() * wordsarray.length)]
-// 	console.log(randomword);
-// }
+	randomword = wordsarray[Math.floor(Math.random() * wordsarray.length)]
+	console.log(randomword);
+}
+
+
+
+var secondsleft = 30;
+var countdown = setInterval(timer, 1000);
+
+function timer() {
+	secondsleft=secondsleft-1;
+	if (secondsleft <=0) {
+		clearInterval(countdown);
+		return;
+
+	}
+
+	document.getElementById('timer').innerHTML=secondsleft + 'seconds';
+}

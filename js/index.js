eaab2b1ba5b0b85c9ae71d713b82c7a9f4b3e8ca
Tech-A41 <tech-a41@Tech-A41s-MacBook-Air.local>
2018-02-27 10:37:29 +0000

    change the random word in h2

--- js/index.js
+++ js/index.js
@@ -10,54 +10,61 @@ var firstword = Math.floor(Math.random()*wordsarray.length);
 // change the heading2 text to have a random word from array
 var arraytoheading = $("h2").html(wordsarray[firstword]);
 
+// restart game
+var restartgame = false;
 
+// start game
+var startgame = true;
 
 // get the user input from HTML
 document.querySelector('form.wordform').addEventListener('submit', function(event) {
 	event.preventDefault();
 	var value = word.value;
+	word.value = '';
 
+
+	// create a variable for the user to match the word
 	var randomtext = $(arraytoheading).html();
 
-	console.log(value);
-	console.log(randomtext);
+	function newwordfromarray() {
+		$('h2').html(wordsarray[Math.floor(Math.random()*wordsarray.length)]);
+
+		// var newrandom = arraytoheading;
+		// var newrandomtext = randomtext;
+	}
 
 
+	// be sure the correct value and random text is matching
 	// match user input to h2
-
 	if (value === randomtext) {
 		alert('Well done');
 		//alert(value.match(arraytoheading));
+		console.log(value);
+		newwordfromarray();
 	}
 	else{ 
 		alert('Fail');
 	}
-
 	// clearing the input box
 	//word.value = "";
+
 });
 
+// countdown timer from 30 seconds
 
-
-// generate random word from array 
-
-// for (var i = 0; i < wordsarray.length; i++){
-
-// 	randomword = wordsarray[Math.floor(Math.random() * wordsarray.length)]
-// 	console.log(randomword);
-// }
-
-
-
-var secondsleft = 30;
-var countdown = setInterval(timer, 1000);
+var secondsleft = 10;
+var countdown = setInterval(timer, 1000); // set interval to 1 second
 
 function timer() {
-	secondsleft=secondsleft-1;
+	secondsleft=secondsleft-1; // minus one second from the timer
 	if (secondsleft <=0) {
 		clearInterval(countdown);
-		return;
-
+		// alert the user time is up and can no longer continue
+		for(i = secondsleft+1; i > 0; i--) {
+		alert('time is up');
+		word.value = true;
+		$('h2').html('End of game');
+}
 	}
 	//$('#timer').html('Timer');
 
